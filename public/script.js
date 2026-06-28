4cd68154abe82854a9f82482ba47c2e08c6804ef
Taquidira <claujactaquidir@gmail.com>
2026-06-28 22:09:38 +0200

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -114,7 +114,7 @@ userDiv.appendChild(meta);
 
         await new Promise(resolve => setTimeout(resolve, 1200));
 
-        const res = await fetch("https://chatbotiisctem.onrender.com/chat", {
+        const res = await fetch("https://chatbotIsctem-2.onrender.com/chat", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ message })
@@ -164,7 +164,7 @@ function getTime() {
 // AQUI VAI A FUNÇÃO createFaq
 async function createFaq(question, id) {
 
-    await fetch("https://chatbotiisctem.onrender.com/add-faq", {
+    await fetch("https://chatbotIsctem-2.onrender.com/add-faq", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
@@ -241,7 +241,7 @@ async function deleteFaq(id) {
 
     if (!confirmDelete) return;
 
-    const res = await fetch(`https://chatbotiisctem.onrender.com/faq/${id}`, {
+    const res = await fetch(`https://chatbotIsctem-2.onrender.com/faq/${id}`, {
         method: "DELETE"
     });
 
